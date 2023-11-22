f876f0965e5b4e0ab598932d0036a17a5c994569
Rony Ken <ronyknagai@gmail.com>
2023-11-22 18:42:54 -0300

    Section home - construcão

--- src/components/MainHome/MainHome.jsx
+++ src/components/MainHome/MainHome.jsx
@@ -3,6 +3,8 @@ import  Container from 'react-bootstrap/Container';
 import  Col  from 'react-bootstrap/Col';
 import  Button from 'react-bootstrap/Button';
 import "./MainHome.scss"
+import Soul1 from '../../assets/soul-1-png.png'
+import MainSection from '../MainSection/MainSection';
 export default function MainHome() {
 
     return(
@@ -14,22 +16,24 @@ export default function MainHome() {
                             <span className='h_subTitle'>Saúde e Praticidade</span>
                             <div className="w-75 mt-2 h_title">
                                 <h1 style={{ color: '#03658C' }}>Soul</h1>
-                                <h1 style={{ color: '#3D3F42' }}>Nosso Dispositivo</h1>
+                                <h1 style={{ color: '#3D3F42' }}>Dispositivo</h1>
                                 <h1 style={{ color: '#3D3F42' }}>De Monitoramento Remoto</h1>
                             </div>
                             <p className='text-secondary'>
-                                Discover a world of delicious salad at our online salad paradise. salad meals are ready to eat in a minutes so save time energy.
+                                Descubra um novo mundo de saúde remota, com muita mais praticidade, facilidade e segurança na palma da sua mão.
                             </p>
-                            <div className="d-flex justify-content-between align-items-start w-50 mt-4">
-                                <Button className='bg_login fw-bold border-0'>order food</Button>
+
+                            {/* Talvez se der tempo eu faça o modal para a pessoa pre-ordar */}
+                            {/* <div className="d-flex justify-content-between align-items-start w-50 mt-4">
+                                <Button className='bg_login fw-bold border-0'>Pre-order!</Button>
                                 <Button className='contact_btn' href='/aboutus'>Contact Us</Button>
-                            </div>
+                            </div> */}
                         </div>
                     </Col>
                     <Col md={6}>
                         <div className="head_right">
                             <div className="imageContainer d-flex justify-content-end align-items-center shadow-sm rounded">
-                                {/* <img src={head_rightImg} alt="headerr-image" className='head_rightImg' /> */}
+                                <img src={Soul1} alt="Soul-imagem" className='head_rightImg' />
                             </div>
                         </div>
                     </Col>
@@ -44,8 +48,8 @@ export default function MainHome() {
                         {/* <img src={coock} alt="coock" className='' style={{ width: '90%', height: '100%', objectFit: 'cover' }} /> */}
                         </div>
                         <div className="w-100">
-                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Delicious Food</h5>
-                        <p className='text-secondary'>Delicious FoodLorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, non!</p>
+                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Monitoramento Remoto</h5>
+                        <p className='text-secondary'>Seja capaz de monitorar o seu dispositivo Soul, ou de algum familiar através do seu celular!</p>
                         </div>
                     </div>
                     </div>
@@ -57,8 +61,8 @@ export default function MainHome() {
                         {/* <img src={delivery} alt="coock" className='' style={{ width: '90%', height: '100%', objectFit: 'cover' }} /> */}
                         </div>
                         <div className="w-100">
-                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Fast Delivery</h5>
-                        <p className='text-secondary'>Delicious FoodLorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, non!</p>
+                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Em Tempo Real</h5>
+                        <p className='text-secondary'>Com sensores top de linha do mercado, o Soul é capaz de mostrar seus sinais vitais extremamente rapido!</p>
                         </div>
                     </div>
                     </div>
@@ -70,17 +74,16 @@ export default function MainHome() {
                         {/* <img src={thumbs} alt="coock" className='' style={{ width: '90%', height: '100%', objectFit: 'cover' }} /> */}
                         </div>
                         <div className="w-100">
-                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Feedback
-                            <small className='ms-3 fw-normal'>
-                            <i className="fa-solid fa-star starr me-1"></i>
-                            4.8 (14.5k)
-                            </small></h5>
-                        <p className='text-secondary'>Delicious FoodLorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, non!</p>
+                        <h5 style={{ color: '#3D3F42', fontWeight: 'bold' }}>Praticidade</h5>
+                        <p className='text-secondary'>O Soul também alerta automaticamente quando algum dos sensores captam valores fora do comum, e se continuar por um determinado período de tempo, ele já agenda uma consulta para você!</p>
                         </div>
                     </div>
                     </div>
                 </Col>
                 </Row>
+                <div className="headerFoot my-4">
+                    <MainSection/>
+                </div>
             </Container>
         </div>
 
