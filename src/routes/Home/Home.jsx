6b0e1732a8ac0170ba43419b1ce4a4312db695e2
Rony Ken <ronyknagai@gmail.com>
2023-11-22 16:19:45 -0300

    Começo da landing page

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header.jsx';
 import Footer from '../../components/Footer.jsx';
+import MainHome from "../../components/MainHome.jsx"
 
 export default function Home(){
     return(
         <>
             <Header/>
+            <MainHome/>
             <Footer/>
         </>
     );
